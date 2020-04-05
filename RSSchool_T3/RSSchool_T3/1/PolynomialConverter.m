b9abd8fda21e30d3e71d0398c69738fef0160f17
Dmitriy S <sutyginds@gmail.com>
2020-04-05 19:04:48 +0400

    feat: 1 and 2 task done

--- RSSchool_T3/RSSchool_T3/1/PolynomialConverter.m
+++ RSSchool_T3/RSSchool_T3/1/PolynomialConverter.m
@@ -2,6 +2,45 @@
 
 @implementation PolynomialConverter
 - (NSString*)convertToStringFrom:(NSArray <NSNumber*>*)numbers {
-    return @"";
+    
+    NSMutableString *polinomial = [[NSMutableString alloc] init];
+    if (numbers.count == 0) {
+        return nil;
+    }
+    
+    for (int i = 0; i < numbers.count; i++) {
+        
+        int currentValue = [numbers[i] intValue];
+        
+        if (currentValue == 0) {
+            continue;
+        };
+        
+        if (currentValue > 0 && i != 0){
+            [polinomial appendString:@" + "];
+        } else if (currentValue < 0) {
+            [polinomial appendString:@" - "];
+        }
+        
+        if (currentValue != 1 && currentValue != -1) {
+            if (currentValue < 0) {
+                currentValue = currentValue * -1;
+            }
+            [polinomial appendString:[NSString stringWithFormat:@"%d", currentValue]];
+        }
+        if (i < numbers.count-1) {
+            [polinomial appendString:@"x"];
+        }
+        if (i < numbers.count-2) {
+            [polinomial appendString:@"^"];
+            [polinomial appendString: [[NSNumber numberWithUnsignedLong:(numbers.count - i - 1)] stringValue]];
+        };
+        
+    }
+    
+    NSLog(@"resul - %@", polinomial);
+    
+    return polinomial;
 }
 @end
+
--- RSSchool_T3/RSSchool_T3/2/Combinator.m
+++ RSSchool_T3/RSSchool_T3/2/Combinator.m
@@ -2,6 +2,36 @@
 
 @implementation Combinator
 - (NSNumber*)chechChooseFromArray:(NSArray <NSNumber*>*)array {
-    return @0;
+    
+    int choice = 1;
+    
+    while ([[self getCombination:array[1] with:[NSNumber numberWithInt:choice]] intValue] != [array[0] intValue] && [array[1] intValue] >= choice) {
+        choice++;
+    }
+    
+    return ([array[1] intValue] < choice) ? nil : [NSNumber numberWithInt:choice];
 }
+
+-(NSNumber*) getCombination:(NSNumber *) elements with:(NSNumber *) choice {
+    
+    int intElements = [elements intValue];
+    int intChoice = [choice intValue];
+    
+    long combination = [self factorial:intElements] / ([self factorial:(intElements - intChoice)] * [self factorial:intChoice]);
+    
+    return [NSNumber numberWithLong:combination];
+}
+
+-(long) factorial: (int) n {
+    long result = 1;
+    if (n == 0 || n == 1) {
+        return 1;
+    }
+    for (int i = 1; i <= n; i++){
+        result = result * i;
+    }
+    
+    return result;
+}
+
 @end
